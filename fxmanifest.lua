21225726a6e071d36e2db3fa3299a8d34944f2d8
i-kulgu <i.kulgu@gmail.com>
2022-04-01 14:58:33 +0200

    initial

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 shared_script 'config.lua'
-client_script 'client.lua'
-server_scripts 'server.lua'
+client_script 'client/main.lua'
+server_scripts 'server/main.lua'
 
 lua54 'yes'
