e60800c55dd7656592365cd5b395cd6f52894957
i-kulgu <i.kulgu@gmail.com>
2022-11-03 16:20:00 +0100

    update

--- client/main.lua
+++ client/main.lua
@@ -1,24 +1,36 @@
-local mask = false
-ESX = nil
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent("esx:getSharedObject", function(library)
-			ESX = library
-		end)
-		Citizen.Wait(0)
-	end
-end)
+local QBCore = exports['qb-core']:GetCoreObject()
+local wearing = false
+local masks = Config.masks
 
-RegisterNetEvent('ik-masks:client:wearMask')
-AddEventHandler('ik-masks:client:wearMask', function(maskId)
-    mask = not mask
-    if mask then
-        loadAnimDict('mp_masks@standard_car@ds@')
-        TaskPlayAnim(PlayerPedId(), 'mp_masks@standard_car@ds@', 'put_on_mask', 8.0, 8.0, 800, 16, 0, false, false, false)
-        SetPedComponentVariation(PlayerPedId(), 1, maskId, 0, 1)
-    else
-        loadAnimDict('mp_masks@standard_car@ds@')
-        TaskPlayAnim(PlayerPedId(), 'mp_masks@standard_car@ds@', 'put_on_mask', 8.0, 8.0, 800, 16, 0, false, false, false)
-        SetPedComponentVariation(PlayerPedId(), 1, 0, 0, 1)
+RegisterNetEvent('masks:client:wear', function(itemName)
+    local ped = PlayerPedId()
+    if not wearing then
+        QBCore.Functions.Progressbar("puton_mask", "Putting mask on..", 2500, false, true, {
+            disableMovement = false,
+            disableCarMovement = false,
+            disableMouse = false,
+            disableCombat = true,
+        }, {
+            animDict = 'mp_masks@standard_car@ds@',
+            anim = 'put_on_mask',
+            flags = 16,
+        }, {}, {}, function() -- Done
+            SetPedComponentVariation(ped, 1, masks[itemName].variation, masks[itemName].palette)
+            wearing = true
+        end)
+    elseif wearing then
+        QBCore.Functions.Progressbar("putoff_mask", "Putting mask off..", 2500, false , true, {
+            disableMovement = false,
+            disableCarMovement = false,
+            disableMouse = false,
+            disableCombat = true,
+        }, {
+            animDict = 'mp_masks@standard_car@ds@',
+            anim = 'put_on_mask',
+            flags = 16,
+        }, {}, {}, function() -- Done
+            SetPedComponentVariation(ped, 1, 0, 0)
+            wearing = false
+        end)
     end
 end)
